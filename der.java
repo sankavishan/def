61ef01227b57dd79b3061c6a8ccf0b6a69f927eb
sankavishan <sankaviece123@gmail.com>
2016-09-19 19:19:32 +0530

    Create der.java

---
+++ der.java
@@ -0,0 +1,14 @@
+import java.util.*;
+Class def
+{
+public static void main(Stringargs[])
+{
+Scanner ob=new Scanner(System.in);
+int a=ob.nextInt();
+while(a!=0)
+{
+count++;
+}
+System.out.printf(count);
+}
+}
